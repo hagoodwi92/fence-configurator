686b77f0d474f293ccaa2b199a539bf0ddeb3d5d
hagoodwin <hagoodwi@gmail.com>
2022-03-07 15:52:20 -0800

    validation

--- fence-config/src/App.js
+++ fence-config/src/App.js
@@ -26,12 +26,21 @@ export default function App() {
     event.height = parseInt(event.height);
     event.walkGate = parseInt(event.walkGate);
     event.doubleGate = parseInt(event.doubleGate);
-    console.log(event.totalFeet, event.accent, event.postInstall, event.height)
-    setFeet(Math.ceil(((event.totalFeet/6)*event.height) + (event.walkGate*13) +(event.doubleGate*26) + event.accent));
-    setPosts(Math.ceil((event.totalFeet/6)+1));
-    setBrackets(Math.ceil(event.totalFeet/6));
+    console.log(event.totalFeet, event.accent, event.postInstall, event.height);
+    setFeet(
+      Math.ceil(
+        (event.totalFeet / 6) * event.height +
+          event.walkGate * 13 +
+          event.doubleGate * 26 +
+          event.accent
+      )
+    );
+    setPosts(Math.ceil(event.totalFeet / 6 + 1));
+    setBrackets(Math.ceil(event.totalFeet / 6));
     setBlackAlum(event.blackAlum);
-    setGateFrame(Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))); 
+    setGateFrame(
+      Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
+    );
   };
 
   const [posts, setPosts] = useState(0);
@@ -44,63 +53,75 @@ export default function App() {
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <React.Fragment>
       <Container>
-        {/* <img class='logo' src={logo1}></img> */}
-        <h1>Infinity Euro Fencing Calculator</h1>
-        <br></br>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          Total Feet:{" "}
-          <input
-            defaultValue="0"
-            {...register("totalFeet", { required: true })}
-          />
-          <select defaultValue="0" {...register("height")}>
-            <option value="8">4'</option>
-            <option value="13">6'</option>
-          </select>
-          {/* <select defaultValue="0" {...register("postInstall")}>
+          {/* <img class='logo' src={logo1}></img> */}
+          <h1>Infinity Euro Fencing Calculator</h1>
+          <br></br>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* register your input into the hook by invoking the "register" function */}
+            Total Feet:{" "}
+            <input
+              defaultValue="0"
+              {...register("totalFeet", { required: true })}
+            />
+            <select defaultValue="0" {...register("height")}>
+              <option value="8">4'</option>
+              <option value="13">6'</option>
+            </select>
+            {/* <select defaultValue="0" {...register("postInstall")}>
             <option value="mount">Mount</option>
             <option value="ground">Ground</option>
           </select> */}
-          <select defaultValue="0" {...register("accent")}>
-            <option value="-3">Lattice</option>
-            <option value="-3">Acrilic</option>
-            <option value="0">None</option>
-          </select>
-          Black Alum:
-          <input defaultValue="0" {...register("blackAlum")} />
-          Walk Gate:
-          <input defaultValue="0" {...register("walkGate")} />
-          Double Gate:
-          <input defaultValue="0" {...register("doubleGate")} />
-          {/* include validation with required or other standard HTML validation rules */}
-          {/* <input {...register("exampleRequired", { required: true })} /> */}
-          {/* errors will return when field validation fails  */}
-          {errors.totalFeet && <span>This field is required</span>}
-          <input type="submit" />
-        </form>
-        <br></br>
-        <br></br>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <u>Parts Needed:</u>
-            </TableHead>
-            <TableRow>
-              <TableCell>Composite Boards: {totalFeet}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Posts: {posts}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Brackets: {brackets}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Gate Frames: {gateFrame}</TableCell>
-            </TableRow>
-            <TableCell>Black Alum Boards: {blackAlum}</TableCell>
-          </Table>
-        </TableContainer>
+            <select defaultValue="0" {...register("accent")}>
+              <option value="-3">Lattice</option>
+              <option value="-3">Acrilic</option>
+              <option value="0">None</option>
+            </select>
+            Black Alum:
+            <input
+              defaultValue="1"
+              {...register("blackAlum", { required: true })}
+            />
+            Walk Gate:
+            <input
+              defaultValue="0"
+              {...register("walkGate", { required: true })}
+            />
+            Double Gate:
+            <input
+              defaultValue="0"
+              {...register("doubleGate", { required: true })}
+            />
+            {/* include validation with required or other standard HTML validation rules */}
+            {/* <input {...register("exampleRequired", { required: true })} /> */}
+            {/* errors will return when field validation fails  */}
+            {errors.blackAlum && <span>This field is required</span>}
+            {errors.totalFeet && <span>This field is required</span>}
+            {errors.walkGate && <span>This field is required</span>}
+            {errors.doubleGate && <span>This field is required</span>}
+            <input type="submit" />
+          </form>
+          <br></br>
+          <br></br>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <u>Parts Needed:</u>
+              </TableHead>
+              <TableRow>
+                <TableCell>Composite Boards: {totalFeet}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Posts: {posts}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Brackets: {brackets}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Gate Frames: {gateFrame}</TableCell>
+              </TableRow>
+              <TableCell>Black Alum Boards: {blackAlum}</TableCell>
+            </Table>
+          </TableContainer>
       </Container>
     </React.Fragment>
   );
