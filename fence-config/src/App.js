15d7643100b40221d721202ae4a9fda376f5c827
hagoodwin <hagoodwi@gmail.com>
2022-04-27 11:31:06 -0700

    only shows parts needed

--- fence-config/src/App.js
+++ fence-config/src/App.js
@@ -26,10 +26,10 @@ import oxfordGrey from "./oxfordGrey.jpeg";
 import kingCedar from "./kingCedar.jpeg";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
-import singleGate from "./singleGate.jpg";
-import oxford_grey from "./oxford_grey.jpg";
-import black_rose from "./black_rose.jpg";
-import king_cedar from "./king_cedar.jpg";
+// import singleGate from "./singleGate.jpg";
+// import oxford_grey from "./oxford_grey.jpg";
+// import black_rose from "./black_rose.jpg";
+// import king_cedar from "./king_cedar.jpg";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -47,21 +47,21 @@ export default function App() {
     formState: { errors },
   } = useForm();
   const onReset = (event) => {
-    setBlack(0);
-    setCedar(0);
-    setGrey(0);
-    setBrackets(0);
-    setGateFrame(0);
-    setBlackAlum(0);
+    setBlack();
+    setCedar();
+    setGrey();
+    setBrackets();
+    setGateFrame();
+    setBlackAlum();
     setsixFtPosts();
     seteightFtPosts();
-    setfourFtPosts(0);
-    setDropRod(0);
-    setSurfaceMount(0);
-    setAcrylic(0);
-    setLattice(0);
-    setSixFtHDPosts(0);
-    setEightFtHDPosts(0);
+    setfourFtPosts();
+    setDropRod();
+    setSurfaceMount();
+    setAcrylic();
+    setLattice();
+    // setSixFtHDPosts(0);
+    // setEightFtHDPosts(0);
   };
   const onSubmit = (event) => {
     console.log(event);
@@ -80,48 +80,91 @@ export default function App() {
     event.height = parseInt(event.height);
     event.walkGate = parseInt(event.walkGate);
     event.doubleGate = parseInt(event.doubleGate);
-    setDropRod(event.doubleGate);
+
+    if (event.dropRod > 0) {
+      setDropRod(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Drop Rods (EF 61608): {event.doubleGate}
+              <br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
+    }
     event.blackAlum = parseInt(
       Math.ceil((event.totalFeet / 6) * event.blackAlum)
     );
 
     if (event.color === "black rose") {
       setBlack(
-        Math.max(
-          0,
-          Math.ceil(
-            (event.totalFeet / 6) * event.height -
-              event.walkGate * 13 -
-              event.doubleGate * 26 +
-              accent -
-              event.blackAlum
-          )
-        )
+        <TableRow>
+          <TableCell>
+            <h6>
+              Composite Boards (Black Rose) <i></i>(EF 00200):
+              {Math.max(
+                0,
+                Math.ceil(
+                  (event.totalFeet / 6) * event.height -
+                    event.walkGate * 13 -
+                    event.doubleGate * 26 +
+                    accent -
+                    event.blackAlum
+                )
+              )}
+              {/* <Zoom style={{ float: "right" }}>
+                <img width="50px" src={black_rose} alt="black rose"></img>
+              </Zoom> */}
+            </h6>
+          </TableCell>
+        </TableRow>
       );
     } else if (event.color === "king cedar") {
       setCedar(
-        Math.max(
-          0,
-          Math.ceil(
-            (event.totalFeet / 6) * event.height -
-              event.walkGate * 13 -
-              event.doubleGate * 26 +
-              accent -
-              event.blackAlum
-          )
-        )
+        <TableRow>
+          <TableCell>
+            <h6>
+              Composite Boards (King Cedar) <i></i>(EF 00200):{" "}
+              {Math.max(
+                0,
+                Math.ceil(
+                  (event.totalFeet / 6) * event.height -
+                    event.walkGate * 13 -
+                    event.doubleGate * 26 +
+                    accent -
+                    event.blackAlum
+                )
+              )}
+              {/* <Zoom style={{ float: "right" }}>
+                <img width="50px" src={king_cedar} alt="king cedar"></img>
+              </Zoom> */}
+            </h6>
+          </TableCell>
+        </TableRow>
       );
     } else if (event.color === "oxford grey") {
       setGrey(
-        Math.max(
-          Math.ceil(
-            (event.totalFeet / 6) * event.height -
-              event.walkGate * 13 -
-              event.doubleGate * 26 +
-              accent -
-              event.blackAlum
-          )
-        )
+        <TableRow>
+          <TableCell>
+            <h6>
+              Composite Boards (Oxford Grey) <i></i>(EF 00200):
+              {Math.max(
+                Math.ceil(
+                  (event.totalFeet / 6) * event.height -
+                    event.walkGate * 13 -
+                    event.doubleGate * 26 +
+                    accent -
+                    event.blackAlum
+                )
+              )}
+              {/* <Zoom style={{ float: "right" }}>
+                <img width="50px" src={oxford_grey} alt="oxford grey"></img>
+              </Zoom> */}
+            </h6>
+          </TableCell>
+        </TableRow>
       );
     }
 
@@ -147,60 +190,151 @@ export default function App() {
         </TableRow>
       );
     } else if (event.postInstall === "mount" && event.height === 9) {
-      setfourFtPosts(Math.ceil(event.totalFeet / 6 + 1));
-      setSurfaceMount(Math.ceil(event.totalFeet / 6 + 1));
+      setfourFtPosts(
+        <TableRow>
+          <TableCell>
+            <h6>
+              4' Surface Mount Posts (EF 20108):{" "}
+              {Math.ceil(event.totalFeet / 6 + 1)} <br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
+      setSurfaceMount(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Surface Mounts (EF 30038): {Math.ceil(event.totalFeet / 6 + 1)}{" "}
+              <br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
     } else if (event.postInstall === "mount" && event.height === 14) {
-      setsixFtPosts(Math.ceil(event.totalFeet / 6 + 1));
-      setSurfaceMount(Math.ceil(event.totalFeet / 6 + 1));
+      setsixFtPosts(
+        <TableRow>
+          <TableCell>
+            <h6>6' Posts (EF 20408): {Math.ceil(event.totalFeet / 6 + 1)}</h6>
+          </TableCell>
+        </TableRow>
+      );
+      setSurfaceMount(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Surface Mounts (EF 30038): {Math.ceil(event.totalFeet / 6 + 1)}
+              <br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
     }
-    setBrackets(Math.ceil(event.totalFeet / 6));
-    setBlackAlum(event.blackAlum);
-    setGateFrame(
-      Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
+    setBrackets(
+      <TableRow>
+        <TableCell>
+          <h6>
+            Framing Bracket (EF 40408): {Math.ceil(event.totalFeet / 6)}
+            <br></br>
+            <br></br>
+          </h6>
+        </TableCell>
+      </TableRow>
     );
+
+    if (event.blackAlum > 0) {
+      setBlackAlum(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Aluminum Accent Boards (EF 00308): {event.blackAlum}
+              <br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
+    }
+    if (event.walkGate > 0 || event.doubleGate > 0) {
+      setGateFrame(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Gate Frames (EF):
+              {Math.ceil(
+                parseInt(event.walkGate) + parseInt(event.doubleGate * 2)
+              )}
+              <br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
+    }
     if (event.accent === "lattice") {
-      setLattice(1);
+      setLattice(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Lattice Accent (EF 50308): 1<br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
     } else if (event.accent === "acrylic") {
-      setAcrylic(1);
+      setAcrylic(
+        <TableRow>
+          <TableCell>
+            <h6>
+              Acrylic Accent (EF 51309): 1<br></br>
+              <br></br>
+            </h6>
+          </TableCell>
+        </TableRow>
+      );
     }
 
-    if (event.walkGate > 0 || event.doubleGate > 0) {
-      if (event.postInstall === "ground" && event.height === 9) {
-        setSixFtHDPosts(
-          Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
-        );
-      } else if (event.postInstall === "ground" && event.height === 14) {
-        setEightFtHDPosts(
-          Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
-        );
-      } else if (event.postInstall === "mount" && event.height === 14) {
-        setSixFtHDPosts(
-          Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
-        );
-      } else if (event.postInstall === "mount" && event.height === 9) {
-        setSixFtHDPosts(
-          Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
-        );
-      }
-    }
+    // if (event.walkGate > 0 || event.doubleGate > 0) {
+    //   if (event.postInstall === "ground" && event.height === 9) {
+    //     setSixFtHDPosts(
+    //       Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
+    //     );
+    //   } else if (event.postInstall === "ground" && event.height === 14) {
+    //     setEightFtHDPosts(
+    //       Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
+    //     );
+    //   } else if (event.postInstall === "mount" && event.height === 14) {
+    //     setSixFtHDPosts(
+    //       Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
+    //     );
+    //   } else if (event.postInstall === "mount" && event.height === 9) {
+    //     setSixFtHDPosts(
+    //       Math.ceil(parseInt(event.walkGate) + parseInt(event.doubleGate * 2))
+    //     );
+    //   }
+    // }
   };
 
   const [sixFtPosts, setsixFtPosts] = useState();
   const [eightFtPosts, seteightFtPosts] = useState();
-  const [fourFeetPosts, setfourFtPosts] = useState(0);
-  const [brackets, setBrackets] = useState(0);
-  const [blackAlum, setBlackAlum] = useState(0);
-  const [gateFrame, setGateFrame] = useState(0);
-  const [dropRod, setDropRod] = useState(0);
-  const [surfaceMount, setSurfaceMount] = useState(0);
+  const [fourFeetPosts, setfourFtPosts] = useState();
+  const [brackets, setBrackets] = useState();
+  const [blackAlum, setBlackAlum] = useState();
+  const [gateFrame, setGateFrame] = useState();
+  const [dropRod, setDropRod] = useState();
+  const [surfaceMount, setSurfaceMount] = useState();
   const [submit, setSubmit] = useState(false);
-  const [black, setBlack] = useState(0);
-  const [cedar, setCedar] = useState(0);
-  const [grey, setGrey] = useState(0);
-  const [acrylic, setAcrylic] = useState(0);
-  const [lattice, setLattice] = useState(0);
-  const [sixFtHDPosts, setSixFtHDPosts] = useState(0);
-  const [eightFtHDPosts, setEightFtHDPosts] = useState(0);
+  const [black, setBlack] = useState();
+  const [cedar, setCedar] = useState();
+  const [grey, setGrey] = useState();
+  const [acrylic, setAcrylic] = useState();
+  const [lattice, setLattice] = useState();
+  // const [sixFtHDPosts, setSixFtHDPosts] = useState(0);
+  // const [eightFtHDPosts, setEightFtHDPosts] = useState(0); 
+
 
   function goBack() {
     setSubmit();
@@ -231,144 +365,21 @@ export default function App() {
                     <TableHead>
                       <h3>Parts Needed:</h3>
                     </TableHead>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Composite Boards (Black Rose) <i></i>(EF 00200):{" "}
-                          <br></br>
-                          <br></br>
-                          {black.toLocaleString()}
-                          <br></br>
-                          <br></br>
-                          <Zoom style={{ float: "right" }}>
-                            <img
-                              width="50px"
-                              src={black_rose}
-                              alt="black rose"
-                            ></img>
-                          </Zoom>
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Composite Boards (King Cedar) <i></i>(EF 00200):
-                          <br></br>
-                          <br></br>
-                          {cedar.toLocaleString()}
-                          <br></br>
-                          <br></br>
-                          <Zoom style={{ float: "right" }}>
-                            <img
-                              width="50px"
-                              src={king_cedar}
-                              alt="king cedar"
-                            ></img>
-                          </Zoom>
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Composite Boards (Oxford Grey) <i></i>(EF 00200):{" "}
-                          <br></br>
-                          <br></br>
-                          {grey.toLocaleString()}
-                          <br></br>
-                          <br></br>
-                          <Zoom style={{ float: "right" }}>
-                            <img
-                              width="50px"
-                              src={oxford_grey}
-                              alt="oxford grey"
-                            ></img>
-                          </Zoom>
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Framing Bracket (EF 40408): <br></br>
-                          <br></br>
-                          {brackets.toLocaleString()}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Gate Frames (EF 60408): <br></br>
-                          <br></br>
-                          {gateFrame.toLocaleString()}
-                          <br></br>
-                          <Zoom style={{ float: "right" }}>
-                            <img
-                              width="50px"
-                              src={singleGate}
-                              alt="single gate"
-                            ></img>
-                          </Zoom>
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Aluminum Accent Boards (EF 00308): <br></br>
-                          <br></br>
-                          {blackAlum.toLocaleString()}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
+                    {black}
+                    {cedar}
+                    {grey}
+                    {acrylic}
+                    {lattice}
+                    {gateFrame}
+                    {blackAlum}
+                    {brackets}
                     {sixFtPosts}
                     {eightFtPosts}
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          4' Surface Mount Posts (EF 20108): <br></br>
-                          <br></br> {fourFeetPosts.toLocaleString()}{" "}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Drop Rods (EF 61608): <br></br>
-                          <br></br>
-                          {dropRod.toLocaleString()}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Surface Mounts (EF 30038): <br></br>
-                          <br></br> {surfaceMount.toLocaleString()}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Lattice Accent (EF 50308):<br></br>
-                          <br></br>
-                          {lattice.toLocaleString()}{" "}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell>
-                        <h6>
-                          Acrylic Accent (EF 51309): <br></br>
-                          <br></br>
-                          {acrylic.toLocaleString()}
-                        </h6>
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
+                    {fourFeetPosts}
+                    {surfaceMount}
+                    {dropRod}
+
+                    {/* <TableRow>
                       <TableCell>
                         <h6>
                           8' Ft HD Post Kit (EF 62508): <br></br>
@@ -385,7 +396,7 @@ export default function App() {
                           {sixFtHDPosts.toLocaleString()}
                         </h6>
                       </TableCell>
-                    </TableRow>
+                    </TableRow> */}
                     <TableRow>
                       <TableCell>
                         <div id="goBack">
